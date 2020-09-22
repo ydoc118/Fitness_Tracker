488daccc4417917e1eac5578bdce30ed93836c8f
Cody <ydoc118@gmail.com>
2020-09-22 13:31:03 -0400

    added mongoAtlas

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
